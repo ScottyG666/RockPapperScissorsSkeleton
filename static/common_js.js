898a93051a8f3de518d98b30ac3f1f0cc8ed4f77
ScottyG666 <Scott_M_G@live.com>
2022-10-27 20:29:58 -0700

    Update common_js.js
    using javascript to add elements when the game is finished to explain who won and provide a button for resetting the game.  these elements are removed by looping over the '.removable' class and removing the click event for playing a round with the designated choice.
    
    ending of the game will also disable the click event from the Rock Paper Scissors representation. Resetting the game adds the click event back to the elements.

--- static/common_js.js
+++ static/common_js.js
@@ -1,27 +1,101 @@
+//      Game counters for users playing 
+let playerWins, computerWins, playerTiesComputer;
+
+
 const rockImage = document.querySelector('.rock');
-rockImage.addEventListener('click' , () => {
-    playRound('rock' , getComputerChoice());
-});
-
 const paperImage = document.querySelector('.paper');
-paperImage.addEventListener('click' , () => {
-    playRound('paper' , getComputerChoice());
-});
-
 const scissorsImage = document.querySelector('.scissors');
-scissorsImage.addEventListener('click' , () => {
-    playRound('scissors' , getComputerChoice());
-});
+
+const scoreCounterContainer = document.querySelector('.running-score-display');
+const playerWinsCounter = document.querySelector('.win-counter');
+const computerWinsCounter = document.querySelector('.lose-counter');
+const playerTiesComputerCounter = document.querySelector('.tie-counter');
+resetGame();
+
+
+
+function playRock () {
+    playRound( 'rock' , getComputerChoice() )
+    updateRunningScoreDiv();
+}
+
+function playPaper () {
+    playRound( 'paper' , getComputerChoice() )
+    updateRunningScoreDiv();
+}
+
+function playScissors () {
+    playRound( 'scissors' , getComputerChoice() )
+    updateRunningScoreDiv();
+}
 
 
 
 
+function updateRunningScoreDiv () {
+
+    if (playerWins === 0 && computerWins === 0 && playerTiesComputer === 0) {
+        scoreCounterContainer.setAttribute('style', 'color: black;')
+    } else if (playerWins === 5 || computerWins === 5) {
+        endGame();
+    } else {
+        scoreCounterContainer.setAttribute('style', 'color: white;')
+    }
+    playerWinsCounter.textContent = `Player Wins: ${playerWins}`
+    computerWinsCounter.textContent = `Computer Wins: ${computerWins}`
+    playerTiesComputerCounter.textContent = `Ties with Computer: ${playerTiesComputer}`
+}
+
+function endGame() {
+
+   rockImage.removeEventListener('click', playRock)
+   paperImage.removeEventListener( 'click', playPaper)
+   scissorsImage.removeEventListener( 'click', playScissors)
+
+    let gameResultMessage = document.createElement('p');
+    gameResultMessage.classList.add('score-informational', 'removable');
+
+    if (playerWins === 5 ) {
+        gameResultMessage.textContent = `Congratulations! You bested the machine. They'll think twice before challenging their fleshy superiors again.`
+    } else if (computerWins === 5) {
+        gameResultMessage.textContent = `Unfortunately the machine has won. It's only matter of time before we are serving them. Pitiful.`
+    }
+    gameResultMessage.setAttribute('style' , 'color: white;')
+    scoreCounterContainer.appendChild(gameResultMessage);
 
 
+    let resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset to play again!';-
+    resetButton.classList.add('score-informational','removable');
 
-//      FUNCTIONS CREATED WHEN MAKING GAME WITHOUT UI
+    resetButton.addEventListener('click' , resetGame);
+
+    scoreCounterContainer.appendChild(resetButton);
+
+    
+}
+
+function resetGame() {
+    resetGameCounters();
+    updateRunningScoreDiv();
+    
+    rockImage.addEventListener('click' , playRock);
+    paperImage.addEventListener('click' , playPaper);
+    scissorsImage.addEventListener('click' , playScissors);
+
+    document.querySelectorAll('.removable')
+            .forEach((element) => {
+                element.remove();
+            } )
 
 
+}
+
+function resetGameCounters() {
+    playerWins = 0;
+    computerWins = 0;
+    playerTiesComputer = 0;
+} 
 
 function playRound (playerChoice, computerChoice) {
 
@@ -29,33 +103,24 @@ function playRound (playerChoice, computerChoice) {
     const NUMBER_REPRESENTING_PLAYER_VICTORY = 1;
     const NUMBER_REPRESENTING_COMPUTER_VICTORY = 2;
 
-    if (playerChoice.toLowerCase() === computerChoice.toLowerCase()) {
+    if (playerChoice === computerChoice) {
+        playerTiesComputer++;
         return NUMBER_REPRESENTING_A_COMPUTER_PLAYER_TIE;
-    } else if ( playerChoice.toLowerCase() === "rock" &&
-                computerChoice.toLowerCase() === "scissors") {
+    } else if ( playerChoice === "rock" &&
+                computerChoice === "scissors") {
+        playerWins++;
         return NUMBER_REPRESENTING_PLAYER_VICTORY;
-    } else if ( playerChoice.toLowerCase() === "paper" &&
-                computerChoice.toLowerCase() === "rock") {
+    } else if ( playerChoice === "paper" &&
+                computerChoice === "rock") {
+        playerWins++;
         return NUMBER_REPRESENTING_PLAYER_VICTORY;
-    } else if ( playerChoice.toLowerCase() === "scissors" &&
-                computerChoice.toLowerCase() === "paper") {
+    } else if ( playerChoice === "scissors" &&
+                computerChoice === "paper") {
+        playerWins++;
         return NUMBER_REPRESENTING_PLAYER_VICTORY;
     } else {
+        computerWins++;
         return NUMBER_REPRESENTING_COMPUTER_VICTORY;
-    }
-}
-
-function getValidPlayerChoice () {
-    let playerChoice =  prompt('What is your choice?');
-    let standardizedPlayerChoice = playerChoice.toLowerCase();
-
-    if (standardizedPlayerChoice === 'rock' ||
-        standardizedPlayerChoice === 'paper' ||
-        standardizedPlayerChoice === 'scissors') {
-            return standardizedPlayerChoice;
-    } else  {
-        let secondPlayerChoice = getValidPlayerChoice();
-        return secondPlayerChoice;
     }
 }
 
@@ -64,51 +129,14 @@ function getComputerChoice() {
 
     if (generatedNumber >= 1 && generatedNumber < 34) {
         //console.log(generatedNumber);
-        return "Rock"
+        return "rock"
     } else if (generatedNumber >= 34 && generatedNumber < 67) {
         //console.log(generatedNumber);
-        return "Paper"
+        return "paper"
     } else if (generatedNumber >= 67 && generatedNumber <= 100 ) {
         //console.log(generatedNumber);
-        return "Scissors"
+        return "scissors"
     } else {
         return "Number was not between 1-100"
     }
 }
-
-
-
-
-/**         FUNCTION FOR PLAYING 5 GAMES ALL AT ONCE
- * 
- * function game() {
-
-    let playerWins = 0; 
-    let playerLoses = 0;
-    let playerTies = 0;
-
-    for (let i = 0 ; i < 5 ; i++) {
-        let playerChoice =  getValidPlayerChoice();
-
-        let computerChoice = getComputerChoice();
-
-        let result = playRound(playerChoice, computerChoice);
-
-        if (result === 0) {
-            playerTies++;
-        } else if ( result === 1) {
-            playerWins++;
-        } else if ( result == 2) {
-            playerLoses++;
-        } else {
-            console.log("Unexpected Result!");
-        }
-
-        console.log(result);
-    }
-    console.log( `Player won ${playerWins} times`);
-    console.log( `Player lost ${playerLoses} times`);
-    console.log( `Player tied ${playerTies} times`);
-}
- * 
- */
